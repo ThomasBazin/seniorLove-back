48a979ff5dd842f55d1229b655c13a8939fb8658
ThomasBazin <tom.bazin@gmail.com>
2024-09-01 21:43:38 +0200

    deleted a few comments in Scrypt.js config

--- src/auth/Scrypt.js
+++ src/auth/Scrypt.js
@@ -4,9 +4,9 @@ export class Scrypt {
   static hash(password) {
     // On crée le sel : 16 bytes est la recommandation.
     const salt = randomBytes(16).toString('hex');
-    // On crée le hash du mot de passe 64 caractères de long, on peut faire plus ou moins, mais plus c'est mieux : consultez la doc pour les options. Il est retourné sous forme de Buffer <https://nodejs.org/api/buffer.html>
+    // Doc <https://nodejs.org/api/buffer.html>
     // The settings recommended by OWASP do work out to a rather high maxmem:
-    // "If Argon2id is not available, use scrypt with a minimum CPU/memory cost parameter of (2^17)
+    // Use scrypt with a minimum CPU/memory cost parameter of (2^17)
     // a minimum block size of 8 (1024 bytes), and a parallelization parameter of 1"
     // That works out to 134220800, around 134MB.
     const buf = scryptSync(password, salt, 64, {
@@ -25,7 +25,7 @@ export class Scrypt {
     // On hash le mot de passe reçu du req.body avec le sel du mot de passe que l'on a en BDD
 
     // The settings recommended by OWASP do work out to a rather high maxmem:
-    // "If Argon2id is not available, use scrypt with a minimum CPU/memory cost parameter of (2^17)
+    // Use scrypt with a minimum CPU/memory cost parameter of (2^17)
     // a minimum block size of 8 (1024 bytes), and a parallelization parameter of 1"
     // That works out to 134220800, around 134MB.
     const clearPasswordBuffer = scryptSync(plainTextpassword, salt, 64, {
