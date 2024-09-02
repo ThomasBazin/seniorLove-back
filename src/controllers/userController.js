e67ed9f83ca1e21fc0b56e8bfdf1df2e46a0d7de
Widi MARTIAL <widi.martial@gmail.com>
2024-09-02 15:01:16 +0200

    config Joischema

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,8 +1,24 @@
 import { User, Hobby, Event, User_message } from '../models/associations.js';
+import { Scrypt } from '../src/auth/Scrypt.js';
+import Joi from 'joi';
 
+const schema = Joi.object({
+  name: Joi.string().alphanum().max(50).required(),
+  birth_date: Joi.date().less('now').greater('now - 60 years').iso().required(),
+  description: Joi.string().alphanum(),
+  gender: Joi.string().max(10).valid('male', 'female', 'other').required(),
+  picture: Joi.string().max(255),
+  email: Joi.string()
+    .alphanum()
+    .max(255)
+    .email({ minDomainSegments: 2, tlds: { allow: ['com', 'net', 'fr'] } })
+    .required(),
+  password: Joi.string().min(12).max(255).required(),
+  repeat_password: Joi.ref('password'),
+});
 
 //Ajouter un utilisateur
-export async function addUser(req, res){}
+export async function addUser(req, res) {}
 
 //Récupérer tous les utilisateurs
 export async function getAllUsers(req, res) {
@@ -10,7 +26,7 @@ export async function getAllUsers(req, res) {
   //TODO : gestion du 403 unauthorized (token)
 
   res.status(200).json(allUsers);
-};
+}
 
 //Récupérer un utilisateur
 export async function getOneUser(req, res) {
@@ -27,35 +43,34 @@ export async function getOneUser(req, res) {
     res.status(404).json({ message: 'user not found' });
   }
   res.status(200).json(oneUser);
-};
+}
 
 //Récuperer l'utilisateur connecté
-export async function getConnectedUser(req, res){}
+export async function getConnectedUser(req, res) {}
 
 //Mettre à jour un utilisateur
-export async function updateUser(req, res){}
+export async function updateUser(req, res) {}
 
 //Supprimer un utilisateur
-export async function deleteUser(req, res){}
+export async function deleteUser(req, res) {}
 
 //Connecter un utilisateur
-export async function loginUser(req, res){}
+export async function loginUser(req, res) {}
 
 //Déconnecter un utilisateur
-export async function logoutUser(req, res){}
+export async function logoutUser(req, res) {}
 
 //Récupérer tous les utilisateurs qui ont les mêmes centres d'intérets
-export async function getAllSameInterestUsers(req, res){}
+export async function getAllSameInterestUsers(req, res) {}
 
 //Enregistré un utilisateur connecté, à un évenement spécifique
-export async function addUserToEvent(req, res){}
+export async function addUserToEvent(req, res) {}
 
 //Supprimer un utilisateur connecté, d'un évenement spécifique
-export async function deleteUserToEvent(req, res){}
+export async function deleteUserToEvent(req, res) {}
 
-//Récupérer tous les évenements auquels s'est inscrit un utilisateur 
-export async function getAllEventsUser(req, res){}
+//Récupérer tous les évenements auquels s'est inscrit un utilisateur
+export async function getAllEventsUser(req, res) {}
 
 //Récupere tous les messages d'un utilisateur connecté
-export async function getAllUserMessages(req, res){}
-
+export async function getAllUserMessages(req, res) {}
