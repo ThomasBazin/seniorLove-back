346983896753f026cc433e8d5527741acd9d9b15
Oliva <olivadruyer34@gmail.com>
2024-09-09 12:14:21 +0200

    edit pics profile

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
     "birth_date" DATE NOT NULL,
     "description" TEXT,
     "gender" VARCHAR(10) NOT NULL,
-    "picture" VARCHAR(255),
+    "picture" TEXT,
     "email" VARCHAR(255) UNIQUE NOT NULL,
     "password" VARCHAR(255) NOT NULL,
     "status" VARCHAR(10) DEFAULT 'pending',
