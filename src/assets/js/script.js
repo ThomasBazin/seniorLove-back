a7a3536125b0ba5762c3046c161335f160322214
LevequeKevin <126432986+LevequeKevin@users.noreply.github.com>
2024-09-11 12:06:39 +0200

    refactor: Remove unused code and optimize user status update and deletion
    Co-authored-by: Coada Emilian <emilian.coada@gmail.com>

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', function () {
   userSubmitButton();
   userCancelButton();
   userDeleteButton();
-  hobbiesCheckboxes();
   eventDeleteButton();
   eventCancelButton();
   eventSubmitButton();
@@ -42,26 +41,25 @@ document.addEventListener('DOMContentLoaded', function () {
       const userId = userSubmitButton.getAttribute('data-user-id');
       userSubmitButton.addEventListener('click', function (event) {
         event.preventDefault();
-        // Perform the fetch request to update user status
-        fetch(`/admin/users/${userId}/status`, {
-          method: 'PATCH',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify({ status: statusValue }),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log('Success:', data);
-            // Optionally, update the UI or redirect
-          })
-          .catch((error) => {
+        console.log('button clicked');
+        async function updateUserStatus(userId, statusValue) {
+          try {
+            const response = await fetch(`/admin/users/${userId}/status`, {
+              method: 'PATCH',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+              body: JSON.stringify({ status: statusValue }),
+            });
+            return await response.json();
+          } catch (error) {
             console.error('Error:', error);
-            // Optionally, show an error message
-          });
+          }
+        }
+        updateUserStatus(userId, statusValue);
         setTimeout(() => {
           location.reload();
-        }, 200);
+        }, 50);
       });
     }
   }
@@ -83,51 +81,27 @@ document.addEventListener('DOMContentLoaded', function () {
     if (userDeleteButton) {
       const userId = userDeleteButton.getAttribute('data-user-id');
       userDeleteButton.addEventListener('click', function (event) {
-        event.preventDefault(); // Prevent the default link behavior
-        // Confirm the deletion
-        fetch(`/admin/users/${userId}/delete`, {
-          method: 'DELETE',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-        })
-          .then((response) => {
+        event.preventDefault();
+        async function deleteUser(userId) {
+          try {
+            const response = await fetch(`/admin/users/${userId}/delete`, {
+              method: 'DELETE',
+              headers: {
+                'Content-Type': 'application/json',
+              },
+            });
             if (response.ok) {
               window.location.href = '/admin/users'; // Redirect to users list
             } else {
-              return response.json().then((data) => {
-                throw new Error(data.message || 'Failed to delete user');
-              });
+              const data = await response.json();
+              throw new Error(data.message || 'Failed to delete user');
             }
-          })
-          .catch((error) => {
+          } catch (error) {
             console.error('Error:', error);
             alert('Failed to delete user.');
-          });
-      });
-    }
-  }
-
-  // User page hobby checkbox behavior
-  function hobbiesCheckboxes() {
-    const hobbiesCheckboxes = document.querySelectorAll('.hobby-checkbox');
-    // Check if hobbiesCheckboxes is defined and not null
-    if (hobbiesCheckboxes) {
-      // Iterate over each checkbox element in the hobbiesCheckboxes NodeList
-      hobbiesCheckboxes.forEach(function (checkbox) {
-        // Add an event listener to each checkbox that listens for changes
-        checkbox.addEventListener('change', function () {
-          // Create an array of checked checkboxes
-          const checkedHobbies = Array.from(hobbiesCheckboxes)
-            // Filter the checkboxes to include only those that are checked
-            .filter(function (checkbox) {
-              return checkbox.checked;
-            })
-            // Map the filtered checkboxes to their values
-            .map(function (checkbox) {
-              return checkbox.value;
-            });
-        });
+          }
+        }
+        deleteUser(userId);
       });
     }
   }
