9559b4172b999e5a1324cb0f445a5b3532393df9
LevequeKevin <126432986+LevequeKevin@users.noreply.github.com>
2024-09-12 22:13:08 +0200

    fix: sql script picture type

--- data/create_tables.sql
+++ data/create_tables.sql
@@ -11,7 +11,7 @@ CREATE TABLE users (
     "birth_date" DATE NOT NULL,
     "description" TEXT,
     "gender" VARCHAR(10) NOT NULL,
-    "picture" VARCHAR(255),
+    "picture" TEXT,
     "picture_id" VARCHAR(255),
     "email" VARCHAR(255) UNIQUE NOT NULL,
     "password" VARCHAR(255) NOT NULL,
