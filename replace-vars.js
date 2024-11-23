f202e97f4b753bbabfbe353d558a39e64d9f2049
ThomasBazin <tom.bazin@gmail.com>
2024-11-23 13:09:02 +0100

    refacto db scripts

--- replace-vars.js
+++ replace-vars.js
@@ -24,16 +24,12 @@ async function replaceVars() {
         `psql ${pgUrl} -f data/create_tables.sql`;
       packageJson.scripts['db:populate'] =
         `node data/populate_tables_users.js && psql ${pgUrl} -f data/populate_tables_1.sql && psql ${pgUrl} -f data/populate_tables_2.sql`;
-      packageJson.scripts['db:reset'] =
-        `pnpm run db:create && node data/populate_tables_users.js && psql ${pgUrl} -f data/populate_tables_1.sql && psql ${pgUrl} -f data/populate_tables_2.sql`;
     } else if (process.env.NODE_ENV === 'development') {
       // Update the dev scripts in package.json
       packageJson.scripts['db:create'] =
         `psql -U ${dbUser} -d ${dbName} -f data/create_tables.sql`;
       packageJson.scripts['db:populate'] =
         `node data/populate_tables_users.js && psql -U ${dbUser} -d ${dbName} -f data/populate_tables_1.sql && psql -U ${dbUser} -d ${dbName} -f data/populate_tables_2.sql`;
-      packageJson.scripts['db:reset'] =
-        `pnpm run db:create && node data/populate_tables_users.js && psql -U ${dbUser} -d ${dbName} -f data/populate_tables_1.sql && psql -U ${dbUser} -d ${dbName} -f data/populate_tables_2.sql`;
     }
 
     // Write the updated package.json back to file
