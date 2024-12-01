787fbd9a92e1a07c492d7559b3af66e8667ab078
ThomasBazin <tom.bazin@gmail.com>
2024-12-01 20:38:59 +0100

    test pg SSL

--- data/pgClient.js
+++ data/pgClient.js
@@ -1,17 +1,5 @@
 import 'dotenv/config';
 import pg from 'pg';
 const { Client } = pg;
-let config;
-if (process.env.NODE_ENV === 'development') {
-  config = process.env.PG_URL;
-} else if (process.env.NODE_ENV === 'production') {
-  config = {
-    connectionString: process.env.PG_URL,
-    // Beware! The ssl object is overwritten when parsing the connectionString
-    ssl: {
-      rejectUnauthorized: true,
-    },
-  };
-}
 
-export const pgClient = new Client(config);
+export const pgClient = new Client(process.env.PG_URL);
